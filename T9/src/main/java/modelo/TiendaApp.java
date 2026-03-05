930048f03ff7504fce13ff6d3b1d5264059c0b25
Pablo-gg9 <pgordogonzalez@gmail.com>
2026-03-05 13:01:41 +0100

    refactor: extracción de métodos para mejorar legibilidad

--- T9/src/main/java/modelo/TiendaApp.java
+++ T9/src/main/java/modelo/TiendaApp.java
@@ -5,6 +5,36 @@ import java.util.List;
 
 public class TiendaApp {
 
+    public static void mostrarPedido(List <String> pedidos){
+        
+     for(String iteracion : pedidos ){
+         String[] partes = iteracion.split(",");
+             String nombre = partes[0];
+            String producto = partes[1];
+            double precio = Double.parseDouble(partes[2]);
+            System.out.println("Cliente: " + nombre);
+            System.out.println("Producto: " + producto);
+            System.out.println("Precio: " + precio);
+            System.out.println("-------------------");
+     
+    }
+    }
+    public static double calcularPrecio(List<String> pedidos){
+        double total = 0.0;    
+        for (String iteracion : pedidos) {
+               String[] partes = iteracion.split(",");
+               double precio = Double.parseDouble(partes[2]);
+               total += precio;
+            }
+        return total;
+    }
+    public static double aplicarDescuento(double total, double precioDescuento, double descuento){
+    if (total > precioDescuento) {
+            total -= (total * descuento);
+        }
+        
+    return total;
+    }
     public static void main(String[] args) {
 
         List<String> pedidos = new ArrayList();
@@ -13,31 +43,23 @@ public class TiendaApp {
         pedidos.add("Juan,Raton,20");
         pedidos.add("Juan,Teclado,50");
 
-        double total = 0;
+       
         final double precioDescuento=500;
         final double descuento =0.15;
         
-        for(String iteracion : pedidos ){
-                
-            String[] partes = iteracion.split(",");
+       
+            
 
-            String nombre = partes[0];
-            String producto = partes[1];
-            double precio = Double.parseDouble(partes[2]);
-
-            total = total + precio;
-
-            System.out.println("Cliente: " + nombre);
-            System.out.println("Producto: " + producto);
-            System.out.println("Precio: " + precio);
-            System.out.println("-------------------");
-        }
+         
+            mostrarPedido(pedidos);
+            
+          
+        
+        
 
 
-        if (total > precioDescuento) {
-            total = total - (total * descuento);
-        }
+        
 
-        System.out.println("TOTAL FINAL: " + total);
+        System.out.println("TOTAL FINAL: " + aplicarDescuento(calcularPrecio(pedidos),precioDescuento,descuento));
     }
 }
